507c33b17bb8e94dc12edd082bbd1df92da8e5cd
Shouchuang Zhu <zscjsw@gmail.com>
2019-06-16 22:32:10 -0500

    Registration backend finished‘

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,21 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 
-const bodyParser = require('body-parser');
-
-const items = require('./routes/api/items')
 const app = express();
 
-//Bodyparser Middleware
+// Connect Database
+connectDB();
 
-app.use(bodyParser.json());
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-const db = 'mongodb+srv://Zhu:zscxxx2008.@cluster0-8ajbt.mongodb.net/test?retryWrites=true&w=majority'
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-//mongo connection
-mongoose.connect(db, {useNewUrlParser: true}).then(()=>{
-    console.log('MongoDB Connected')
-}).catch(error => console.log(error));
 
-app.use('/api/items', items);
+const PORT = process.env.PORT || 5000;
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, ()=>{
-    console.log(`Server started on port ${port}`)
-})
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
